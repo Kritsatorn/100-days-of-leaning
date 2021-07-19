bfde1d73b642f78c9bbd0ecc2c1ba22fd306dbf5
Kritsatorn <32409782+Kritsatorn@users.noreply.github.com>
2021-07-19 14:52:25 +0700

    Day5_END

--- js-fundamentals-functional-v2/Day5_Closure.js
+++ js-fundamentals-functional-v2/Day5_Closure.js
@@ -1,15 +1,51 @@
-const myAlert = () => {
-  const x = "Help! I think I found a clue!";
-  let count = 0;
-  const alerter = () => {
-    alert(`${x} ${++count}`);
-  };
+// const myAlert = () => {
+//     const x = "Help! I think I found a clue!";
+//     let count = 0;
+//     const alerter = () => {
+//         alert(`${x} ${++count}`);
+//     };
 
-  return alerter;
+//     return alerter;
+// };
+
+// const funcAlert = myAlert();
+// const funcAlert2 = myAlert();
+// funcAlert();
+// funcAlert();
+// it goone be counted
+
+// Closure is function inside function
+
+//curring
+_ = {};
+_.curry = (fn) => {
+    return (arg1) => {
+        return (arg2) => {
+            return fn(arg1, arg2);
+        };
+    };
 };
 
-const funcAlert = myAlert();
-const funcAlert2 = myAlert();
-funcAlert();
-funcAlert();
-// it goone be counted
+const addList = (num1, num2) => {
+    return [num1, num2];
+};
+console.log(_.curry(addList)([1, 2, 3])(2));
+
+const consider = (name) => {
+    return `I think it could be ${name}`
+}
+
+const exclaim = (statement) => {
+    return `${statement.toUpperCase()} !`
+}
+
+_.compose = (fn1, fn2) => {
+    return (name) => {
+        return fn1(fn2(name));
+    }
+}
+const blame = _.compose(consider, exclaim);
+console.log(blame("you"))
+
+
+// END OF JS FUND
